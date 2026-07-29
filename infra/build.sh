1f7f7cd00ea524316c1191468a773e10f8c82733
Hugh Powell <hugh@hughpowell.co.uk>
2026-07-29 19:41:58 +1000

    Script merging of branches up until release

--- infra/build.sh
+++ infra/build.sh
@@ -2,14 +2,29 @@
 set -e
 set -o errexit
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+
+if [ "$branch" != "main" ]; then
+  echo "Running tests on $branch ..."
+  clj -X:test
+  echo "Success!"
+  echo "Generating documentation ..."
+  clj -X:release documentation/generate
+  echo "Success!"
+  echo "Merging into main ..."
+  git checkout main
+  git merge $branch
+  echo "Success!"
+fi
+
 echo "Cleaning up from last time ..."
 clj -T:infra clean
 echo "Success!"
-echo "Running tests ..."
+echo "Running tests on main..."
 clj -X:test
 echo "Success!"
-echo "Generating documentation ..."
-clj -X:release documentation/generate
+echo "Pushing commits to remote..."
+git push origin main
 echo "Success!"
 echo "Building the jar ..."
 clj -T:infra jar
