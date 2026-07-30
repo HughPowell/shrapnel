7c212fdcb2f176134ba12f2188554de9f0068338
Hugh Powell <hugh@hughpowell.co.uk>
2026-07-30 22:02:32 +1000

    Fix automatic updating of docs

--- infra/documentation.clj
+++ infra/documentation.clj
@@ -9,10 +9,9 @@
       (string/join
         \newline
         [(str "## " (string/replace name "*" "\\*"))
-         (format "\\[[source](%s)\\]\n"
-                 (format "%s#L%d"
-                         (string/replace file #".*/src" "../src")
-                         line))
+         (->> line
+              (format "%s#L%d" (format "../src/%s" file))
+              (format "\\[[source](%s)\\]\n"))
          (when arglists (str arglists \newline))
          doc]))
     docs))
@@ -39,31 +38,31 @@
     [(str "# " (name (ns-name ns-symbol)))
      (:doc (meta ns-symbol))]))
 
-(defn commit-changes! [file]
-  (build/git-process {:git-args ["add" "-f" file]})
-  (when (build/git-process {:git-args "diff" "--cached" "--numstat"})
-    (build/git-process {:git-args ["commit" "-m" "Update API docs"]})))
+(defn commit-changes! [& files]
+  (build/git-process {:git-args (concat ["add" "-f"] files)})
+  (when (build/git-process {:git-args ["diff" "--cached" "--numstat"]})
+    (build/git-process {:git-args ["commit" "-m" "Update docs"]})))
 
 (defn generate-api-docs [ns-symbol file]
   (->> (generate-docs ns-symbol)
        (->markdown)
        (cons (ns-doc ns-symbol))
        (string/join \newline)
-       (spit file))
-  (commit-changes! file))
+       (spit file)))
 
 (defn update-co-ordinates [file]
   (let [version (build/git-count-revs nil)]
     (as-> file $
       (slurp $)
       (string/replace $
-                      #"\{\:mvn/version \"[^\"]*\"\}"
-                      (format "{:mvn/version \"0.1.%s}\"" version))
+                      #"\:mvn/version\s+\"[^\"]*\""
+                      (format ":mvn/version \"0.1.%s\"" version))
       (string/replace $
-                      #"\[net.hughpowell.shrapnel \"[^\"]*\"\]"
-                      (format "[net.hughpowell.shrapnel \"0.1.%s\"]" version))
+                      #"net.hughpowell.shrapnel\s+\"[^\"]*\""
+                      (format "net.hughpowell.shrapnel \"0.1.%s\"" version))
       (spit file $))))
 
 (defn generate [_]
   (generate-api-docs 'net.hughpowell.shrapnel.money "docs/api.md")
-  (update-co-ordinates "README.md"))
+  (update-co-ordinates "README.md")
+  (commit-changes! "docs/api.md" "README.md"))
